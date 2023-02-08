71e07b9a80532689184efc0b8ff97bc9c156865f
HovoBoryan <111977958+HovoBoryan@users.noreply.github.com>
2023-02-08 11:32:54 +0400

    Add files via upload

---
+++ initialData.js
@@ -0,0 +1,19 @@
+const studentsData = [
+    { name: 'Vaxo' },
+    { name: 'Valod' },
+    { name: 'sckjndscjk' },
+    { name: 'TTTTT' },
+];
+
+const booksData = [
+    { title: '1', author: 1 },
+    { title: '2', author: 2 },
+    { title: '3', author: 3 },
+    { title: '4', author: 4 },
+    { title: '5', author: 5 },
+];
+
+module.exports = {
+    studentsData,
+    booksData,
+};
